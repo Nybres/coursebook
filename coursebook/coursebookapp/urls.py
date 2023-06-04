a63eb39fd8702b935a18c1e7b601953e6d18362e
Pawel <pawelserbyn@wp.pl>
2023-06-04 20:47:08 +0200

    add login and register views

--- coursebook/coursebookapp/urls.py
+++ coursebook/coursebookapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("login", views.login, name="login"),
+    path("register-customer", views.register_customer, name="register_customer"),
+    path("register-company", views.register_company, name="register_company"),
 ]
--- coursebook/coursebookapp/views.py
+++ coursebook/coursebookapp/views.py
@@ -6,3 +6,18 @@ from django.shortcuts import render
 def index(request):
     context = {"foo": "ładunek"}
     return render(request, "pages/index.html", context)
+
+
+def login(request):
+    context = {"foo": "ładunek"}
+    return render(request, "pages/login.html", context)
+
+
+def register_customer(request):
+    context = {"foo": "ładunek"}
+    return render(request, "pages/register-customer.html", context)
+
+
+def register_company(request):
+    context = {"foo": "ładunek"}
+    return render(request, "pages/register-company.html", context)
