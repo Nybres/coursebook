6153fe72750b821cf45049f6cfea59a438ca13e0
Pawel <pawelserbyn@wp.pl>
2023-06-10 12:43:35 +0200

    add alert partial

--- coursebook/coursebookapp/views/account_view.py
+++ coursebook/coursebookapp/views/account_view.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
+@login_required(login_url='login')
 def AccountView(request):
     user = request.user
-    print(user)
     return render(request, "pages/account.html", {"user": user})
--- coursebook/coursebookapp/views/login_view.py
+++ coursebook/coursebookapp/views/login_view.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
+from django.contrib import messages
+
 
 class LoginView(LoginView):
     template_name = "pages/login.html"
 
+    def form_invalid(self, form):
+        messages.error(self.request, "Nieprawidłowy login lub hasło")
+        return super().form_invalid(form)
+
     def get_success_url(self):
+        messages.success(self.request, "Pomyślnie zalogowano")
         return reverse_lazy("account")
