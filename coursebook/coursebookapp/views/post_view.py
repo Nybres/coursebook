b8a599d90495d43cb9401decb74bd1741be3ace3
Pawel <pawelserbyn@wp.pl>
2023-11-12 13:18:40 +0100

    mend

--- coursebook/coursebookapp/views/post_view.py
+++ coursebook/coursebookapp/views/post_view.py
@@ -14,9 +14,19 @@ class PostDetailView(generics.RetrieveAPIView):
 
     def get(self, request, *args, **kwargs):
         instance = self.get_object()
+
         breadcrumbs = [
             ("Coursebook", reverse("home")),
             ("Blog", reverse("blog")),
+        ]
+
+        if instance.categories.exists():
+            first_category = instance.categories.first()
+            breadcrumbs.append(
+                (first_category.name, reverse("blog_category", kwargs={"category_id": first_category.id}))
+            )
+
+        breadcrumbs.append(
             (
                 instance.title,
                 reverse(
@@ -25,8 +35,9 @@ class PostDetailView(generics.RetrieveAPIView):
                         "pk": instance.id,
                     },
                 ),
-            ),
-        ]
+            )
+        )
+
         context = {
             "post": instance,
             "breadcrumbs": breadcrumbs,
